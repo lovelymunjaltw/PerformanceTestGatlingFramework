175e95f98aa0daf411c83620da032e0e5537b3af
LovelyM <lovely.munjal@thoughtworks.com>
2019-08-22 00:09:15 +0530

    lm new simulation added - ChainExampleSimulation

---
+++ src/gatling/simulations/ChainExampleSimulation.scala
@@ -0,0 +1,37 @@
+import io.gatling.core.Predef._
+import io.gatling.core.structure.ScenarioBuilder
+import io.gatling.http.Predef.{http, _}
+
+class ChainExampleSimulation extends Simulation {
+
+  before {
+    println("ChainExampleSimulation is about to start!")
+  }
+
+  val baseURL: String = "http://api.jmeter.ninja/objects.json"
+  val numberOfUsers = (System.getenv("NUMBER_OF_USERS")).toInt
+
+  val httpProtocol = http
+    .baseUrl(baseURL)
+
+  val scn: ScenarioBuilder = scenario("Chain Test")
+    .exec(http("Get all objects")
+      .get("")
+      .check(status.is(200))
+      .check(jsonPath("$.objects[0]").saveAs("firstObject")))
+
+    .exec(http("Get an object:" + "${firstObject}")
+      .get("http://api.jmeter.ninja/objects/"+ "${firstObject}" + ".json")
+      .check(status.is(200))
+      //.check(jsonPath("$.result").is("OK")) // this will pass
+      .check(jsonPath("$.result").is("OKK")) //this should fail
+    )
+
+    setUp(scn.inject(atOnceUsers(numberOfUsers))
+    .protocols(httpProtocol))
+    .assertions(global.successfulRequests.percent.gt(99))
+
+  after {
+    println("ChainExampleSimulation is finished!")
+  }
+}
